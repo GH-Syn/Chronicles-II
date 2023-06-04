67a3d4865bb6f94d44a6521be01c74517ce48c0a
Joshua Rose <joshuarose099@gmail.com>
2023-06-04 18:23:14 +1000

    feat: logger debug implementation
    This commit adds functioality behind the debug command / argument
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- src/__main__.py
+++ src/__main__.py
@@ -16,9 +16,10 @@ python file.py
 A folder level entry allows easier relative path grepping to `res`; removing the need for manual inserts into system path variables.
 """
 
+import sys
 import pygame
 
-from utils.io_handler import get_parser
+from utils.io_handler import get_parser, set_logging_level
 
 parser = get_parser()
 
@@ -28,9 +29,12 @@ parser.add_argument(
     help="Show debugging log level messages for developers",
 )
 
-
 def main():
     args = parser.parse_args()
+
+    if args.debug:
+        set_logging_level(level="DEBUG")
+
     window = pygame.display.set_mode()
 
 if __name__ == "__main__":
