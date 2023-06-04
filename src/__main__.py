39d97cf7bf354a1900fb7697e24e3530a8734305
Joshua Rose <joshuarose099@gmail.com>
2023-06-04 15:03:26 +1000

    implement(util): Add `__main__.py` to `src` directory
    This commit adds the main file to the src directory.
    The reasoning for this is explained in the documentation string of the
    afore-mentioned file.
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

---
+++ src/__main__.py
@@ -0,0 +1,15 @@
+"""
+The main file is simply so the following command can be run:
+
+```bash
+python src
+```
+
+This is preferable to a file-level entry.
+```bash
+cd src
+python file.py
+```
+
+A folder level entry allows easier relative path grepping to `res`; removing the need for manual inserts into system path variables.
+"""
