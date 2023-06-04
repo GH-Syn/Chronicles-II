8129579644b1d71194b85ea7e982eed2b4801a9d
Joshua Rose <joshuarose099@gmail.com>
2023-06-04 15:52:38 +1000

    tests(new): Add tests for torch constructor
    Adds tests for torch constructor
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- test/unit/entities/torch/constructor.py
+++ test/unit/entities/torch/constructor.py
@@ -1,10 +1,38 @@
 import os
 import sys
-
 import unittest
 
+sys.path.insert(0, os.getcwd())
+
+from src.entities.torch import Torch
+
+
+class TestTorchConstructor(unittest.TestCase):
+    def test_str(self):
+        torch = Torch()
+        self.assertIsInstance(torch.color, str)
+
+    def test_null(self):
+        torch = Torch(None)
+        self.assertTrue(torch.color == "yellow")
+
+    def test_cols(self):
+        cols = ["yellow", "blue", "green", "red"]
+        for col in cols:
+            torch = Torch(col)
+            self.assertEqual(col, torch.color)
+
+    def test_valid(self):
+        cols = ["lime", "grey"]
+        for col in cols:
+            with self.assertRaises(ValueError):
+                Torch(col)
+
+    def test_type(self):
+        cols = ["lime", True, 1.4]
+        for col in cols:
+            with self.assertRaises(ValueError):
+                Torch(col)
 
 if __name__ == "__main__":
-    sys.path.insert(0, os.getcwd())
-    import src
     unittest.main()
